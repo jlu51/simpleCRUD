4cecd670fd3272fa2277b82a8140453659046a6b
Jonathan Lu <jonathanlu00@gmail.com>
2021-12-24 13:05:13 -0800

    gitignore test push #1

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
+const { application } = require('express');
 const express = require('express') // 
 
 const app = express();
 
 app.listen(3001, () => {
    console.log("SERVER IS RUNNING");
-})
+});
+
+app.get('/', (req, res) => {
+   res.send("Hello World!")
+});
